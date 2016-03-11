2fa8d57ed9a6a23bea7f212d2f0111ad15472660
Evgeny Golyshev <grga-pitic@yandex.ru>
2016-03-11 11:52:46 +0300

    Package RPC server

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='blackmagic',
+      version='0.1',
+      description='',
+      url='https://bitbucket.org/eugulixes/blackmagic',
+      author='CusDeb Team',
+      maintainer='Evgeny Golyshev',
+      maintainer_email='Evgeny Golyshev <eugulixes@gmail.com>',
+      license='http://www.apache.org/licenses/LICENSE-2.0',
+      scripts=['bin/blackmagic.py'],
+      install_requires=[
+          'chardet',
+          'python-debian'
+      ])
