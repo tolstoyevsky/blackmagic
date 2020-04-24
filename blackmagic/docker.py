e014953666e266f341a16d1fc9f6799200035324
Evgeny Golyshev <eugulixes@gmail.com>
2020-04-25 01:51:30 +0300

    Use Django 2.2

---
+++ blackmagic/docker.py
@@ -0,0 +1,24 @@
+"""Module indented for configuring the service via the environment variables. """
+
+import os
+
+DEBUG = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('PG_DATABASE', 'cusdeb-api'),
+        'USER': os.environ.get('PG_USER', 'postgres'),
+        'PASSWORD': os.environ.get('PG_PASSWORD', 'secret'),
+        'HOST': os.environ.get('PG_HOST', 'localhost'),
+        'PORT': os.environ.get('PG_PORT', '5432'),
+    }
+}
+
+INSTALLED_APPS = [
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+]
+
+# Do not run anything if SECRET_KEY is not set.
+SECRET_KEY = os.environ['SECRET_KEY']
