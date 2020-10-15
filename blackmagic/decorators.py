c14d0eb936246ccadebc6f2205cd47e87604576a
Evgeny Golyshev <eugulixes@gmail.com>
2020-10-15 13:53:34 +0300

    Actualize only_if_initialized docstring

--- blackmagic/decorators.py
+++ blackmagic/decorators.py
@@ -6,10 +6,8 @@ NOT_INITIALIZED = 11
 def only_if_initialized(func):
     """Executes a remote procedure only if the RPC server is initialized. Every
     single remote procedure has to be decorated with only_if_initialized. The
-    exceptions are:
-    * init
-    * get_built_images
-    * get_target_devices_list"""
+    exception is 'init'.
+    """
 
     @wraps(func)
     def wrapper(self, request, *args, **kwargs):
